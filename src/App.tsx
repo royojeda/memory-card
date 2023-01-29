fd110eae7436a0f2c954e89ad461ea16a5d8adec
Dennis Ojeda <royojeda1@gmail.com>
2023-01-30 05:15:47 +0800

    feat: add cards' layout
    Includes varying styles for different screen widths.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import Card from "./components/Card";
+
 export default function App() {
   return (
-    <div className="flex min-h-screen w-screen items-center justify-center">
-      <div>Hello, world!</div>
+    <div className="flex min-h-screen max-w-[100vw] bg-slate-800 text-slate-300">
+      <div className="flex flex-1 items-center justify-center p-8">
+        <div className="grid grid-cols-2 gap-4 sm:grid-cols-4 lg:grid-cols-6 xl:gap-8">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Card.tsx
@@ -0,0 +1,5 @@
+export default function Card() {
+  return (
+    <div className="aspect-square w-28 max-w-xs rounded-md border border-slate-900 bg-slate-700 shadow-md shadow-slate-900 sm:aspect-auto sm:h-44 sm:w-32 md:h-48 md:w-36 xl:h-72 xl:w-44" />
+  );
+}
