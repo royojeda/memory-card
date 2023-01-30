3b7510719d96318b220457a45e9c8c0ab3094f3c
Dennis Ojeda <royojeda1@gmail.com>
2023-01-30 23:06:59 +0800

    feat: make cards clickable

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,8 +6,11 @@ export default function Card(props: CardProps) {
   const { content } = props;
 
   return (
-    <div className="aspect-square w-28 max-w-xs rounded-md border border-slate-900 bg-slate-700 shadow-md shadow-slate-900 sm:aspect-auto sm:h-44 sm:w-32 md:h-48 md:w-36 xl:h-72 xl:w-44">
+    <button
+      type="button"
+      className="aspect-square w-28 max-w-xs rounded-md border border-slate-900 bg-slate-700 shadow-md shadow-slate-900 sm:aspect-auto sm:h-44 sm:w-32 md:h-48 md:w-36 xl:h-72 xl:w-44"
+    >
       {content}
-    </div>
+    </button>
   );
 }
