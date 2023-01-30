59357598c2dc8ce4592f73297cae19a98f24b6de
Dennis Ojeda <royojeda1@gmail.com>
2023-01-30 23:03:54 +0800

    feat: add a content property to the cards
    This content is displayed as a child of the Card component.

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ import Card from "./components/Card";
 export default function App() {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [cards, setCards] = useState([
-    { id: uniqid() },
-    { id: uniqid() },
-    { id: uniqid() },
-    { id: uniqid() },
+    { id: uniqid(), content: "1" },
+    { id: uniqid(), content: "2" },
+    { id: uniqid(), content: "3" },
+    { id: uniqid(), content: "4" },
   ]);
 
   return (
@@ -16,7 +16,7 @@ export default function App() {
       <div className="flex flex-1 items-center justify-center p-8">
         <div className="grid grid-cols-2 gap-4 sm:grid-cols-4 lg:grid-cols-6 xl:gap-8">
           {cards.map((card) => (
-            <Card key={card.id} />
+            <Card key={card.id} content={card.content} />
           ))}
         </div>
       </div>
