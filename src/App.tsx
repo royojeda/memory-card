b01d59186918f2fd50240144991dc43665c0a8b0
Dennis Ojeda <royojeda1@gmail.com>
2023-02-02 01:17:46 +0800

    feat: add a score tracker
    Shows the current game's score and the highest score for the current
    session (however many games have been played).

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,8 @@ export default function App() {
   const [clickedCards, setClickedCards] = useState<
     Array<{ id: string; content: string }>
   >([]);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [score, setScore] = useState({ current: 0, highest: 0 });
 
   const shuffle = <Type,>(inputArray: Type[]): Type[] => {
     const outputArray = [...inputArray];
@@ -41,15 +43,32 @@ export default function App() {
 
   const handleClick = (card: { id: string; content: string }) => {
     if (clickedCards.includes(card)) {
+      setScore({ ...score, current: 0 });
+      setClickedCards([]);
       console.log("Game Over!");
     } else {
+      if (score.current >= score.highest) {
+        setScore({ current: score.current + 1, highest: score.highest + 1 });
+      } else {
+        setScore({ ...score, current: score.current + 1 });
+      }
       setClickedCards([...clickedCards, card]);
     }
   };
 
   return (
     <div className="flex min-h-screen max-w-[100vw] bg-slate-800 text-slate-300">
-      <div className="flex flex-1 items-center justify-center p-8">
+      <div className="flex flex-1 flex-col items-center justify-center gap-8 p-8">
+        <div className="flex w-40 divide-x divide-slate-900 rounded-md bg-slate-700 px-4 py-2 text-center shadow shadow-slate-900">
+          <div className="w-1/2 min-w-fit pr-4 xl:text-lg">
+            <div>Score</div>
+            <div className="text-2xl xl:text-3xl">{score.current}</div>
+          </div>
+          <div className="w-1/2 min-w-fit pl-4 xl:text-lg">
+            <div>Best</div>
+            <div className="text-2xl xl:text-3xl">{score.highest}</div>
+          </div>
+        </div>
         <div className="grid grid-cols-2 gap-4 sm:grid-cols-4 lg:grid-cols-6 xl:gap-8">
           {cards.map((card) => (
             <Card
