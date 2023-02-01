b24bd4b3a675e0bbb845889ed157278bcf673c30
Dennis Ojeda <royojeda1@gmail.com>
2023-02-01 09:18:02 +0800

    feat: animate card scale on click
    Added a delay before calling the onClick callback to allow the animation
    to finish.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,12 +5,17 @@ interface CardProps {
 
 export default function Card(props: CardProps) {
   const { content, onClick } = props;
+  const handleClick: React.MouseEventHandler = (event) => {
+    setTimeout(() => {
+      onClick(event);
+    }, 75);
+  };
 
   return (
     <button
       type="button"
-      onClick={onClick}
-      className="aspect-square w-28 max-w-xs rounded-md border border-slate-900 bg-slate-700 shadow-md shadow-slate-900 sm:aspect-auto sm:h-44 sm:w-32 md:h-48 md:w-36 xl:h-72 xl:w-44"
+      onClick={handleClick}
+      className="aspect-square w-28 max-w-xs rounded-md border border-slate-900 bg-slate-700 shadow-md shadow-slate-900 transition duration-75 active:scale-95 sm:aspect-auto sm:h-44 sm:w-32 md:h-48 md:w-36 xl:h-72 xl:w-44"
     >
       {content}
     </button>
